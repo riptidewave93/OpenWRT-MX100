0acee90dc6ade35704b702aac34d5b20f1e7b340
riptide_wave <chrisrblake93@gmail.com>
2021-05-17 20:56:26 -0500

    initial upload

---
+++ README.md
@@ -0,0 +1,44 @@
+# OpenWRT-MX100
+Bringup for the Cisco Meraki MX100 on OpenWRT.
+
+Note that due to some unsupported hardware, this port will most likely not end up upstream. If you would like to see this fixed, the following drivers would need porting to mainline linux:
+
+* DH89xxCC GPIO Driver
+* NU801 LED Driver
+* DH89xxCC Watchdog Driver
+
+Building
+-----
+#### Build Only
+`./build.sh`
+
+#### Modify Configs and Build
+`./build.sh modify`
+
+Note that you will need to run a modify on the first compile to select the x86 target, x86_64 subtarget in the menuconfig.
+
+Booting
+-----
+
+
+Flashing
+-----
+
+To Do
+-----
+##### MX100
+* LED's/GPIOs
+* Reset button
+* System Integration
+* Watchdog
+
+Working
+-----
+##### MX100
+* Ethernet
+* Sysupgrade Support
+* Kernel Device Profile
+
+Notice
+------
+No promises this won't brick your unit, and no promises that this will even work!
---
+++ build.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+firstbuild=0
+clonedir=./openwrt
+
+# Print messages in cyan blue
+Msg() {
+  echo -e "\e[96m$1\e[39m"
+}
+
+# Do we want menuconfig's and an option to save configs?
+if [ "$1" = "modify" ]; then
+  modify=1
+else
+  modify=0
+fi
+
+Msg "Starting Build Process!"
+
+if [ ! -d "$clonedir" ]; then
+  firstbuild=1
+  Msg "Cloning Repo..."
+  git clone https://github.com/openwrt/openwrt.git $clonedir
+  cd $clonedir
+  git reset --hard 097dc943f1f9b02835c6b249b2a035679da5619f
+  cd - > /dev/null
+fi
+
+if [ "$firstbuild" -eq "1" ] && [ -d "./patches" ]; then
+  Msg "Applying Patches..."
+  cd $clonedir
+  for patch in ../patches/*.patch
+  do
+	  Msg "Applying $patch"
+	  git apply $patch
+  done
+  cd - > /dev/null
+fi
+
+if [ "$firstbuild" -eq "0" ]; then
+  Msg "Cleaning Builddir..."
+  cd $clonedir
+  rm -rf ./bin
+  make clean
+  cd - > /dev/null
+fi
+
+Msg "Applying overlay..."
+cp -R ./overlay/* $clonedir/
+
+if [ "$firstbuild" -eq "1" ]; then
+  Msg "Installing feeds..."
+  cd $clonedir
+  ./scripts/feeds update -a
+  ./scripts/feeds install -a
+  if [ -f "../config/diffconfig" ]; then
+  	Msg "Applying and Expanding config..."
+  	cp ../config/diffconfig ./.config
+  	make defconfig
+  fi
+  cd - > /dev/null
+fi
+
+if [ "$modify" -eq "1" ]; then
+  cd $clonedir
+  Msg "Loading Menuconfig"
+  make menuconfig -j`nproc` V=s
+  cd - > /dev/null
+fi
+
+Msg "Building Time!!!"
+cd $clonedir
+make -j`nproc` V=s
+
+if [ $? -ne 0 ]; then
+  cd - > /dev/null
+  Msg "Build Failed!"
+  exit 1
+else
+  cd - > /dev/null
+  Msg "Compile Complete!"
+fi
+
+Msg "Build.sh Finished!"
